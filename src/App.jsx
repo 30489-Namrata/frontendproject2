80b0d381286030d4b05b3c3df01ca1c6d727d029
30489-Namrata <2400030489@kluniversity.in>
2025-07-30 12:33:19 +0530

    version1

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,14 @@ function App() {
   return (
     <>
       <h1>Welcome To FrontEnd class in KLU</h1>
-      <h2>Today's topic is introduction to React</h2>
+      <h2>Today's topic is introduction to Rgiteact</h2>
       <h3>Today's date is 29.07.2025</h3>
       <h4>VRRA</h4>
-      <h5>2400030522 - SHAIK BUSHAR JAHA</h5>
+      <h5>2400030489 - GUDETI NAMRATA</h5>
       <Link to="/page1">Go To Page1</Link><br />
       <Link to="/page2">Go To Page2</Link><br />
       <Link to="/page3">Go To Page3</Link><br />
+      <Link to="/page4">Go To Page4</Link><br />
     </>
   )
 }
